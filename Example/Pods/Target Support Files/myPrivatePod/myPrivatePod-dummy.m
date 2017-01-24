47232a1312d8af765b27a03af7a17c3dfd95e36c
Jason <jason_chung@17life.com>
2017-01-24 15:48:39 +0800

    檔案移動處理

---
+++ Example/Pods/Target Support Files/myPrivatePod/myPrivatePod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_myPrivatePod : NSObject
+@end
+@implementation PodsDummy_myPrivatePod
+@end
---
+++ Example/Pods/Target Support Files/myPrivatePod/myPrivatePod-umbrella.h
@@ -0,0 +1,22 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "CTItemCollectionCell.h"
+#import "CTItemCollectionViewController.h"
+#import "CTItemListModel.h"
+#import "CTItemModel.h"
+#import "CTItem.h"
+#import "CTItemListComponent.h"
+
+FOUNDATION_EXPORT double myPrivatePodVersionNumber;
+FOUNDATION_EXPORT const unsigned char myPrivatePodVersionString[];
+
